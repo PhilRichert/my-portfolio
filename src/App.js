f65586f53e1c89d15ea3806fffc9812bfc9656d6
Philip Richert <prichert93@web.de>
2022-08-16 15:29:38 +0200

    Created React app and added a simple Navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/navbar";
+import Navigation from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
